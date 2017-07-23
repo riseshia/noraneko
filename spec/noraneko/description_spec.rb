7619f9a3e9f0dd41a8cd85834a81f9dfb5ce4c2f
Shia <rise.shia@gmail.com>
2017-07-23 13:32:33 +0900

    Recogize module description

--- spec/noraneko/description_spec.rb
+++ spec/noraneko/description_spec.rb
@@ -1,35 +1,173 @@
 require 'spec_helper'
 
 RSpec.describe Noraneko::Description do
-  let(:source) do
-    <<-EOC
-    class SimpleClass
-      def used_public
-        external_method
-        used_private
-      end
-
-      def hige
-        used_public + 1
-      end
-
-      private
-
-      def used_private
-      end
-
-      def unused_private
-      end
-    end
-    EOC
-  end
-
   let(:description) { Noraneko::Analyzer.new.execute(source) }
 
+  context '#build_context' do
+    context 'with simple class which has no method' do
+      let(:source) do
+        <<-EOC
+        class SimpleClass
+        end
+        EOC
+      end
+
+      it 'is a class description' do
+        expect(description.type).to eq(:class)
+      end
+
+      it 'has name as "SimpleClass"' do
+        expect(description.name).to eq('SimpleClass')
+      end
+
+      it 'has no public instance method' do
+        expect(description.defined_public_methods).to be_empty
+      end
+
+      it 'has no private instance method' do
+        expect(description.defined_private_methods).to be_empty
+      end
+
+      it 'has no expended module' do
+        expect(description.extended_modules).to be_empty
+      end
+
+      it 'has no included module' do
+        expect(description.included_modules).to be_empty
+      end
+    end
+
+    context 'with complex class' do
+      let(:source) do
+        <<-EOC
+        class ComplexClass
+          include OddEye
+          extend Nekomimi
+
+          def hoge
+          end
+
+          def noge;end
+
+          def self.coge
+          end
+
+          class << self
+           def koge
+           end
+          end
+
+          def moge
+          end
+          private :moge
+
+          private
+
+          def loge
+          end
+        end
+        EOC
+      end
+
+      it 'is a class description' do
+        expect(description.type).to eq(:class)
+      end
+
+      it 'has name as "ComplexClass"' do
+        expect(description.name).to eq('ComplexClass')
+      end
+
+      it 'has 4 public instance method' do
+        expect(description.defined_public_methods).to \
+          eq(%w[coge hoge koge noge])
+      end
+
+      it 'has two private instance method' do
+        expect(description.defined_private_methods).to \
+          eq(%w[loge moge])
+      end
+
+      it 'has one expended module' do
+        expect(description.extended_modules).to eq(%w[Nekomimi])
+      end
+
+      it 'has one included module' do
+        expect(description.included_modules).to eq(%w[OddEye])
+      end
+    end
+
+    context 'with module' do
+      let(:source) do
+        <<-EOC
+        module ComplexModule
+          include OddEye
+          extend Nekomimi
+
+          def hoge
+          end
+
+          module_function
+
+          def hige
+          end
+        end
+        EOC
+      end
+
+      it 'is a module description' do
+        expect(description.type).to eq(:module)
+      end
+
+      it 'has name as "ComplexModule"' do
+        expect(description.name).to eq('ComplexModule')
+      end
+
+      it 'has one public instance method' do
+        expect(description.defined_public_methods).to \
+          eq(%w[hige hoge])
+      end
+
+      it 'has one private instance method' do
+        expect(description.defined_private_methods).to be_empty
+      end
+
+      it 'has one expended module' do
+        expect(description.extended_modules).to eq(%w[Nekomimi])
+      end
+
+      it 'has one included module' do
+        expect(description.included_modules).to eq(%w[OddEye])
+      end
+    end
+  end
+
   context '#using?' do
+    let(:source) do
+      <<-EOC
+      class SimpleClass
+        def used_public
+          external_method
+          used_private
+        end
+
+        def hige
+          used_public + 1
+        end
+
+        private
+
+        def used_private
+        end
+
+        def unused_private
+        end
+      end
+      EOC
+    end
+
     context 'with used method' do
       %w[external_method used_public used_private].each do |method_name|
-        it 'returns true' do
+        it "#{method_name} is used" do
           expect(description.using?(method_name)).to eq(true)
         end
       end
@@ -37,7 +175,7 @@ RSpec.describe Noraneko::Description do
 
     context 'with unused method' do
       %w[unused_public unused_private].each do |method_name|
-        it 'returns false' do
+        it "#{method_name} is not used" do
           expect(description.using?(method_name)).to eq(false)
         end
       end
