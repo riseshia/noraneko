5d37a870fde554b5ac0fa01c61e6a94560bc1795
Shia <rise.shia@gmail.com>
2017-07-30 15:41:04 +0900

    Refine Runner

--- lib/noraneko/runner.rb
+++ lib/noraneko/runner.rb
@@ -4,8 +4,8 @@ module Noraneko
   class Runner
     def run(paths)
       target_files = find_target_files(paths)
-      project = scan_files(target_files)
-      project.all_unuseds
+      registry = analyze_ruby_files(target_files)
+      Project.new(registry).all_unuseds
     end
 
     private
@@ -20,18 +20,17 @@ module Noraneko
       end
     end
 
-    def scan_files(target_files)
-      registry = Noraneko::Registry.new
-
-      target_files.each do |file|
-        file.gsub!('//', '/')
-        processor =
-          Noraneko::Processor.init_with(registry: registry, filepath: file)
-        source = File.read(file)
-        ast = Parser::CurrentRuby.parse(source)
-        processor.process(ast)
+    def analyze_ruby_files(target_files)
+      Noraneko::Registry.new.tap do |registry|
+        target_files.each do |file|
+          file.gsub!('//', '/')
+          processor =
+            Noraneko::Processor.init_with(registry: registry, filepath: file)
+          source = File.read(file)
+          ast = Parser::CurrentRuby.parse(source)
+          processor.process(ast)
+        end
       end
-      Project.new(registry)
     end
   end
 end
